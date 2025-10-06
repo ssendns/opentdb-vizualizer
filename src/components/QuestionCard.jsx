60bf62e5a738a5a799c78fb739ac061b58a8848d
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-10-06 09:41:26 +0300

    add right wrong answer feedback

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,12 +1,21 @@
+import { useState, useMemo } from "react";
 import { decodeHTML } from "../utils/decodeHTML";
 
 export default function QuestionCard({ questionObj }) {
   const { question, category, difficulty, correct_answer, incorrect_answers } =
     questionObj;
+  const [selected, setSelected] = useState(null);
+  const allAnswers = useMemo(() => {
+    return [...incorrect_answers, correct_answer].sort(
+      () => Math.random() - 0.5
+    );
+  }, []);
 
-  const allAnswers = [...incorrect_answers, correct_answer].sort(
-    () => Math.random() - 0.5
-  );
+  const handleAnswerClick = (ans) => {
+    if (!selected) {
+      setSelected(ans);
+    }
+  };
 
   return (
     <div className="bg-white shadow-md rounded-xl p-5 border border-gray-200 w-full max-w-2xl mx-auto">
@@ -30,14 +39,27 @@ export default function QuestionCard({ questionObj }) {
       </h3>
 
       <ul className="space-y-2">
-        {allAnswers.map((ans, i) => (
-          <li
-            key={i}
-            className="border rounded-md px-3 py-2 bg-gray-50 hover:bg-gray-100 text-gray-700 transition"
-          >
-            {decodeHTML(ans)}
-          </li>
-        ))}
+        {allAnswers.map((ans, i) => {
+          const isCorrect = ans === correct_answer;
+          const isSelected = selected === ans;
+
+          let bg = "bg-gray-50 hover:bg-gray-100";
+          if (selected) {
+            if (isSelected && isCorrect) bg = "bg-green-100 border-green-400";
+            if (isSelected && !isCorrect) bg = "bg-red-100 border-red-400";
+            if (!isSelected && isCorrect) bg = "bg-green-50";
+          }
+
+          return (
+            <li
+              key={i}
+              onClick={() => handleAnswerClick(ans)}
+              className={`border rounded-md px-3 py-2 text-gray-700 transition cursor-pointer ${bg}`}
+            >
+              {decodeHTML(ans)}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
